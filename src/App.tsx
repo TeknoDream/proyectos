f260c5780c7b177f09ada1a64b2b9e304eadafdd
TeknoDream <87876520+TeknoDream@users.noreply.github.com>
2026-04-17 16:29:39 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/portafolio">
+      <BrowserRouter basename="/proyectos">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
